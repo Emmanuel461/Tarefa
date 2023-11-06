0834fb5a9248d31f1bd1f8cbe75cdfbe0399b843
Emmanuel461 <jesusc461@gmail.com>
2023-11-06 17:52:53 +0000

    Ajust in the the precision

--- AssigmentP4/myfunctions.py
+++ AssigmentP4/myfunctions.py
@@ -10,7 +10,7 @@ def rad2deg(x):
 # Square root function using bissection method
 # Input: number
 def  mysqrt(x):
-    tol=10**-5 # error tolerance
+    tol=10**-9 # error tolerance ####CHANGE#### Before -5  #### #### #### #### #### #### #### ####
     if x<0:
         return 0
     min=0
@@ -74,7 +74,7 @@ def sin45(x):
 # input: number
 # output: angle in decimal degrees
 def myarcsin(x):
-    tol=10**-5 # error tolerance
+    tol=10**-9 # error tolerance before -5 ####CHANGE### #### #### #### #### #### #### #### ####
     if x <= -1:
         return -90
     if x >= 1:
