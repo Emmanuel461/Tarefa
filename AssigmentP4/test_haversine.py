d6d635886209abc14e718cfc407d857474279f4c
Emmanuel461 <jesusc461@gmail.com>
2023-11-06 00:24:12 +0000

    Test without try

--- AssigmentP4/test_haversine.py
+++ AssigmentP4/test_haversine.py
@@ -9,9 +9,7 @@ def test_haversine():
 
     calc_1 =  myhaversine(lyon, paris)
     calc_2 = haversine(lyon, paris)
-    try:
-        diff = abs(float(calc_1 - calc_2))
-        assert diff <= 0.01
-    except AssertionError:
-        print("La diferencia es más de 0.01 km (10 metros)")
+    diff = abs(calc_1 - calc_2)
+    assert diff <= 0.01, "La diferencia es mayor que 0.01 (10m)"
+
 
